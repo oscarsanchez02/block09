c329c434dda8bdf26853d75b8ccc4a962d43407f
oscarsanchez02 <156387300+oscarsanchez02@users.noreply.github.com>
2024-02-18 19:00:03 -0600

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,1 +1,14 @@
 console.log("Hello World");
+
+const form = document.querySelector('#form')
+const submitButton = document.querySelector('#submit')
+
+form.addEventListener('submit', (e) => {
+  submitButton.disabled = true
+  e.preventDefault()
+  window.location.href = window.location.origin + '/success.html'
+})
+
+setTimeout(() => {
+    window.location.href = window.location.origin
+  }, 5000)
